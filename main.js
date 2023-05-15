2ca0e1612ce5294b949e13e5655bac3422bce30b
Ale Olivati <ale.olivati@gmail.com>
2023-05-15 19:02:47 -0300

    commit to branch exercicio_oo

--- main.js
+++ main.js
@@ -1,19 +1,54 @@
-$(document).ready(function () {
-    const endpoint = 'https://api.github.com/users/Aleolivati'
+function Hero (heroName, heroRace, heroClass, firstHand, secondHand, armor, magicPower) {
+    this.heroName = heroName ;
+    this.heroRace = heroRace ;
+    this.heroClass = heroClass ;
+    this.firstHand = firstHand ;
+    this.secondHand = secondHand ;
+    this.armor = armor ;
 
-    fetch(endpoint)
-    .then(function(answer) {
-        return answer.json() ;
-    })
-    .then(function (json) {
-        console.log(json) ;
+    let _magicAttak = magicPower ;
 
-        $('#avatar-img').attr('src',json.avatar_url) ;
-        $('#name').html(json.name) ;
-        $('#username').html('@' + json.login) ;
-        $('#repositories').html(json.public_repos) ;
-        $('#followers').html(json.followers) ;
-        $('#following').html(json.following) ;
-        $('#link-github').attr('href',json.html_url) ;
-    })
-})
+    this.getMagicPower = function () {
+        return _magicAttak ;
+    }
+
+    this.setMagicPower = function (value) {
+        _magicAttak = value ;
+    }
+}
+
+function DwarfMage (heroName) {
+    Hero.call(this, heroName, 'Dwarf', 'Mage', 'Staf', 'none', 'Robe')
+    this.Int = 20 ;
+    this.MagicDamage = 50 ;
+
+    this.theMagicPower = function () {
+        const valueMagicAttack = this.Int * this.MagicDamage ;
+        this.setMagicPower(valueMagicAttack) ;
+    }
+}
+
+function OrcPaladin (heroName) {
+    Hero.call(this, heroName, 'Orc', 'Paladin', 'Sword', 'Shield', 'Full Plate')
+    this.Str = 20 ; 
+    this.Damage = 50 ;
+
+    this.theMagicPower = function () {
+        const valueMagicAttack = 15 ;
+        this.setMagicPower(valueMagicAttack) ;
+    }
+}
+
+const hero1 = new DwarfMage('Loyen') ;
+const hero2 = new OrcPaladin('Laythan') ;
+const hero3 = new Hero('Suylan', 'Elf', 'Druid', 'Bow', 'none', 'Leather', 20) ;
+
+console.log(hero1) ;
+hero1.theMagicPower() ;
+console.log("The Magic Power is " + hero1.getMagicPower()) ;
+
+console.log(hero2) ;
+hero2.theMagicPower() ;
+console.log("The Magic Power is " + hero2.getMagicPower()) ;
+
+console.log(hero3) ;
