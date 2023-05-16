7266f039787d7290944609626571cd7ae5496ad7
Ale Olivati <ale.olivati@gmail.com>
2023-05-16 16:28:21 -0300

    commit to branch exercicio_es6

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const alunos = ['Gustavo', 'Amanda', 'Luiz', 'Cassandra', 'Margarida', 'José', 'Miguel'] ;
+
+const notas = [9,3,5,8,6,1,10] ;
+
+
+const boletim = alunos.map((nomeAluno, indice) => ({
+        nome: nomeAluno,
+        nota: notas[indice]
+})) ;
+
+boletim.push({
+    nome: 'Maria',
+    nota: 5
+}, {
+    nome: 'Pedro',
+    nota: 8
+})
+
+
+const aprovados = boletim.filter(boletimAluno => {return boletimAluno.nota >= 6 }) ;
+
+console.log(aprovados)
