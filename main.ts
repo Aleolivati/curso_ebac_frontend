fc5af682da1cac5646e87685a4acaa52d45c2749
Ale Olivati <ale.olivati@gmail.com>
2023-05-17 22:03:22 -0300

    commit to branch exercicio_ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
-"use strict";
 // Função Multiplicar:
 // function multiply(numberA: number, numberB: number): number {
 //     return numberA * numberB;
 // }
+
 // Função Multiplicar com Arrow Function:
-const multiply = (numberA, numberB) => numberA * numberB;
-console.log(multiply(10, 5));
+const multiply = (numberA: number, numberB: number):number => numberA * numberB ;
+
+console.log(multiply(10,5))
+
 // Função Saudação:
 // function greeting(name: string): string {
 //     return `Olá ${name}!`
 // }
+
 // Função Saudação com Arrow Function:
-const greeting = (name) => `Olá ${name}!`;
-console.log(greeting('Ale'));
+const greeting = (name: string): string => `Olá ${name}!`;
+
+console.log(greeting('Alê'));
